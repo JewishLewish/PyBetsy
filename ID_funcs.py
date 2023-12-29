97aa6e763a153ef3b22015dd11e452ccb5758934
JewishLewish <lanbrokhman@gmail.com>
2023-12-28 20:40:35 -0800

    test

---
+++ ID_funcs.py
@@ -0,0 +1,77 @@
+class PylintWarning:
+    """
+    Pylint Warnings
+    """
+    def __init__(self, data):
+        self.type = data.get("type")
+        self.module = data.get("module")
+        self.obj = data.get("obj")
+        self.line = data.get("line")
+        self.column = data.get("column")
+        self.endLine = data.get("endLine")
+        self.endColumn = data.get("endColumn")
+        self.path = data.get("path")
+        self.symbol = data.get("symbol")
+        self.message: str or list(str) = data.get("message")
+        self.message_id: str or list(str) = data.get("message-id")
+    
+    def __str__(self):
+        return (
+            f"Type: {self.type}\n"
+            f"Module: {self.module}\n"
+            f"Object: {self.obj}\n"
+            f"Line: {self.line}\n"
+            f"Column: {self.column}\n"
+            f"EndLine: {self.endLine}\n"
+            f"EndColumn: {self.endColumn}\n"
+            f"Path: {self.path}\n"
+            f"Symbol: {self.symbol}\n"
+            f"Message: {self.message}\n"
+            f"Message ID: {self.message_id}"
+        )
+
+class Warnings():
+    def W0611(self, details: PylintWarning):
+        #Unused Import
+        self.code[details.line-1] = ""
+
+class Conventions():
+    def C0304(self, details: PylintWarning):
+        self.code.append('')
+
+    def C0114(self, details: PylintWarning):
+        # #https://pylint.readthedocs.io/en/latest/user_guide/messages/convention/missing-module-docstring.html
+        pass 
+
+    def C0116(self, details: PylintWarning):
+        # https://pylint.pycqa.org/en/latest/user_guide/messages/convention/missing-function-docstring.html
+        pass
+
+    def C0415(self, details: PylintWarning):
+        pass
+
+    def C0103(self, details:PylintWarning):
+        print(details)
+
+class Errors():
+    def E0001(self, details: PylintWarning):
+        self.code[details.line-1] = self.code[details.line-1] + "   # {details.message}"
+
+    def E0602(self, details: PylintWarning):
+        new_string = self.code[details.line-1]
+        new_string = new_string[:details.column] + '...' + new_string[details.endColumn:]
+        new_string = addCommentToEnd(new_string, details.message)
+        self.code[details.line-1] = new_string
+
+
+class Solutions(Warnings, Conventions, Errors):
+    def __init__(self, code) -> None:
+        self.code: list(str) = code
+
+
+def addCommentToEnd(string: str, comment: str):
+    if string[-1] == "\n":
+        return string[:-1] + f"     #{comment}" + string[-1]
+    else:
+        return string + f"     #{comment}"
+    
---
+++ lib.py
@@ -0,0 +1,58 @@
+import json
+import subprocess
+from ID_funcs import Solutions, PylintWarning
+
+TOMBSTONE = "🪦"
+
+class Codegen(Solutions):
+    def __init__(self, code, warnings):
+        super().__init__(code=code)
+        self.warnings: set[PylintWarning] = set(warnings)
+    
+    def removeRepeats(self):
+        new_Warnings = []
+        warnings_data_list = []
+
+        for warning in self.warnings:
+            data = {"line":warning.line, "column":warning.column, "endLine":warning.endLine}
+            if data in warnings_data_list:
+                pass
+            else:
+                warnings_data_list.append(data)
+                new_Warnings.append(warning)
+    
+        self.warnings = set(new_Warnings)
+
+    def review(self):
+        for warning in self.warnings:
+            try:
+                function_reference = getattr(self, warning.message_id)
+                try:
+                    function_reference(warning)
+                except Exception as e:
+                    print(e)
+            except:
+                print("WARNING MESSAGE ID =>", warning.message_id, "<= IS NOT IMPLEMENTED!")
+    
+    def writeTo(self, target_file):
+        with open("new"+target_file, "w") as f:
+            f.write(''.join(code.code))
+
+def run_pylint(args): subprocess.run(["pylint"] + args, capture_output=True, text=True) 
+
+def read_content(target_file) -> str:
+    with open(target_file, "r") as f: return f.read()
+
+def read_python_content(target_file) -> str:
+    with open(target_file, "r") as f: return f.readlines()
+
+if __name__ == "__main__":
+    target_file = "main.py"
+    run_pylint([target_file, "--output-format=json:review.json"])
+    
+    code = Codegen(read_python_content(target_file), set([PylintWarning(message) for message in json.loads(read_content("review.json"))]))
+    code.removeRepeats()
+    code.review()
+    print("================")
+    print(''.join(code.code))
+    code.writeTo(target_file=target_file)
